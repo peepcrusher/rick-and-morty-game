ce90c50fbece813ea29fa04569636d45ad99b4ab
Sam Thompson <electronic62794@yahoo.com>
2019-02-25 17:13:47 -0800

    added styling and functionality

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -21,30 +21,31 @@ var grepples;
 // also sets the user's score to 0, and generates a new number for the computer
 
 function reset(){
-    smeckles = Math.floor(Math.random()*20 +1);
-    flurbos = Math.floor(Math.random()*20 +1);
-    smidgens = Math.floor(Math.random()*20 +1);
-    grepples = Math.floor(Math.random()*20 +1);
+    smeckles = Math.floor(Math.random()*15 +1);
+    flurbos = Math.floor(Math.random()*15 +1);
+    smidgens = Math.floor(Math.random()*15 +1);
+    grepples = Math.floor(Math.random()*15 +1);
     computer = 35 + Math.floor(Math.random()*95 +1);
+    $("#computer").html("Match this: " + computer);
     score = 0
+    $("#score").html("Total *BUUuuRRRrrppp* Munnies:  " + score);
+}
+
+//this function updates the score 
+function score(){
+    $("#score").html("Your Score: " + score);
 }
 
 //get starting values for 1st game
 
 reset();
-console.log(score);
 console.log(smeckles);
-//If the user's score equals the computer's score, add 1 to the win count, and reset
-if(score === computer){
-    win++
-    reset()
-}
+console.log(flurbos);
+console.log(smidgens);
+console.log(grepples);
+console.log(computer);
 
-//If the user's score is greater than the computer's score, add 1 to the loss count, and reset
-if(score > computer){
-    loss++;
-    reset();
-}
+
 
 
 
@@ -54,14 +55,87 @@ if(score > computer){
 //add the value of the smeckles to score
 $("#smeckles").on("click", function(){
     score = score + smeckles;
-    console.log(score);
+    $("#score").html("Total *BUUuuRRRrrppp* Munnies: " + score);
+
+
+    
+    //If the user's score is greater than the computer's score, add 1 to the loss count, and reset
+    if(score > computer){
+        loss++;
+        reset();
+        $("#losses").html("Losses: " + loss);
+    }
+
+       //If the user's score equals the computer's score, add 1 to the win count, and reset 
+    else if(score === computer){
+        win++
+        reset()
+        $("#wins").html("Wins: " + win)
+        $("#win-screen").html("BBBuuuurrrrrppppp. Ya did it!");
+    };
+    
+
 });
 
-console.log(score);
-//Create an onClick event for flurbos
+//Create an onClick event for flurbos, add flurbos to the score, and update score
 
-//create an onclick event for smidgens
+//create an onclick event for smidgens, add smidgens to the score, and update score
+$("#smidgens").on("click", function(){
+    score = score + smidgens;
+    $("#score").html("Total *BUUuuRRRrrppp* Munnies: " + score);
+    if(score > computer){
+        loss++;
+        reset();
+        $("#losses").html("Losses: " + loss);
+    }
 
-//create an onclick event for grepples
+       //If the user's score equals the computer's score, add 1 to the win count, and reset 
+    else if(score === computer){
+        win++
+        reset()
+        $("#wins").html("Wins: " + win)
+        $("#win-screen").html("BBBuuuurrrrrppppp. Ya did it!");
+    };
+});
+
+//create an onclick event for grepples, add grepples to the score, and update score
+
+$("#grepples").on("click", function(){
+    score = score + grepples;
+    $("#score").html("Total *BUUuuRRRrrppp* Munnies: " + score);
+    if(score > computer){
+        loss++;
+        reset();
+        $("#losses").html("Losses: " + loss);
+    }
+
+       //If the user's score equals the computer's score, add 1 to the win count, and reset 
+    else if(score === computer){
+        win++
+        reset()
+        $("#wins").html("Wins: " + win)
+        $("#win-screen").html("BBBuuuurrrrrppppp. Ya did it!");
+    };
+});
 
 
+$("#flurbos").on("click", function(){
+    console.log("test");
+    score = score + flurbos;
+    $("#score").html("Total *BUUuuRRRrrppp* Munnies: " + score);
+    //if user score is greater than 
+    if(score > computer){
+        loss++;
+        reset();
+        $("#losses").html("Losses: " + loss);
+    }
+
+       //If the user's score equals the computer's score, add 1 to the win count, and reset 
+    else if(score === computer){
+        win++
+        reset()
+        $("#wins").html("Wins: " + win)
+        $("#win-screen").html("BBBuuuurrrrrppppp. Ya did it!");
+    };
+
+});
